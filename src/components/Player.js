25897582600e8e9759c374e7005080dacda1afcd
Your Name <newfallout.410@gmail.com>
2023-03-19 10:09:09 +0200

    commit message

--- src/components/Player.js
+++ src/components/Player.js
@@ -14,7 +14,7 @@ const Player = ({item, playersHP}) => {
     return (
         <div>
             <Avatar/>
-            <p>{item.name} {playersHP}\{item.hp}</p>
+            <p>{item.userName} {item.name} {playersHP}\{item.hp}</p>
             <HPBar hp={playersHP} maxHp={item.hp} />
         </div>
     );
