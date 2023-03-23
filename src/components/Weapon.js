074d3b878e30f2ddafbd9611d69feb09cda3a556
Your Name <newfallout.410@gmail.com>
2023-03-23 11:27:56 +0200

    first commit

--- src/components/Weapon.js
+++ src/components/Weapon.js
@@ -5,7 +5,7 @@ const Weapon = (props) => {
 
     return (
         <div className={"newSkill"} onClick={() => props.chooseWeapon(props.id)}  >
-            <img src={props.img} width={128} height={64} alt={"weapon"}></img>
+            <img src={props.img} width={64} height={32} alt={"weapon"}></img>
             <p><b>{props.name}</b></p>
             <p>{props.desc}</p>
         </div>
