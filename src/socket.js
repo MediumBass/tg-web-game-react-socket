c72bf6124b422a647e357c13b2edace600d03cc7
MediumBass <115606680+MediumBass@users.noreply.github.com>
2023-09-29 12:49:33 +0300

    new deploy URL

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client'
-const socket= io("https://tggame-server.herokuapp.com/");
+const socket= io("https://tggame-server-65b0aa39e712.herokuapp.com/");
 
 export default socket;
