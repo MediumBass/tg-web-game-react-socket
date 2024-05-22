dc73c1bd2ab02ab9ad8c2f93167396dd4ce0d518
mediumbass <newfallout.410@gmail.com>
2024-05-22 17:19:15 +0300

    a

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
-export const baseURL="https://tg-web-game-react-socket-server-k9vtezt3n-mediumbass-projects.vercel.app/"
+export const baseURL="http://localhost:8000"
 export const socket= io(baseURL);
 
 
