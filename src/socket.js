dd945cfa15a456e516f59e7440e583bbda446d54
mediumbass <newfallout.410@gmail.com>
2024-05-22 17:24:21 +0300

    a

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
-export const baseURL="http://localhost:8000"
+export const baseURL="https://8587-185-209-58-139.ngrok-free.app"
 export const socket= io(baseURL);
 
 
